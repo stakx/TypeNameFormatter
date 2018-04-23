de17adedb75fff85242386cf9e60159d32fafcb2
stakx <stakx@eml.cc>
2018-04-23 23:03:51 +0200

    Add tests for generic type instantiations

--- src/TypeNameFormatter.Tests/Tests.cs
+++ src/TypeNameFormatter.Tests/Tests.cs
@@ -186,5 +186,149 @@ namespace TypeNameFormatter
         {
             Assert.Equal("N.O.A<T, U>", typeof(global::N.O.A<,>).GetFormattedFullName());
         }
+
+        [Fact]
+        public void Generic_type_instantiation_without_namespace_arg_is_type_without_namespace_Name()
+        {
+            Assert.Equal("A<A>", typeof(global::A<global::A>).GetFormattedName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_without_namespace_arg_is_type_without_namespace_FullName()
+        {
+            Assert.Equal("A<A>", typeof(global::A<global::A>).GetFormattedFullName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_with_namespace_arg_is_type_without_namespace_Name()
+        {
+            Assert.Equal("A<A>", typeof(global::N.A<global::A>).GetFormattedName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_with_namespace_arg_is_type_without_namespace_FullName()
+        {
+            Assert.Equal("N.A<A>", typeof(global::N.A<global::A>).GetFormattedFullName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_with_double_namespace_arg_is_type_without_namespace_Name()
+        {
+            Assert.Equal("A<A>", typeof(global::N.O.A<global::A>).GetFormattedName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_with_double_namespace_arg_is_type_without_namespace_FullName()
+        {
+            Assert.Equal("N.O.A<A>", typeof(global::N.O.A<global::A>).GetFormattedFullName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_without_namespace_arg_is_type_with_namespace_Name()
+        {
+            Assert.Equal("A<A>", typeof(global::A<global::N.A>).GetFormattedName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_without_namespace_arg_is_type_with_namespace_FullName()
+        {
+            Assert.Equal("A<N.A>", typeof(global::A<global::N.A>).GetFormattedFullName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_with_namespace_arg_is_type_with_namespace_Name()
+        {
+            Assert.Equal("A<A>", typeof(global::N.A<global::N.A>).GetFormattedName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_with_namespace_arg_is_type_with_namespace_FullName()
+        {
+            Assert.Equal("N.A<N.A>", typeof(global::N.A<global::N.A>).GetFormattedFullName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_with_double_namespace_arg_is_type_with_namespace_Name()
+        {
+            Assert.Equal("A<A>", typeof(global::N.O.A<global::N.A>).GetFormattedName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_with_double_namespace_arg_is_type_with_namespace_FullName()
+        {
+            Assert.Equal("N.O.A<N.A>", typeof(global::N.O.A<global::N.A>).GetFormattedFullName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_without_namespace_arg_is_type_with_double_namespace_Name()
+        {
+            Assert.Equal("A<A>", typeof(global::A<global::N.O.A>).GetFormattedName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_without_namespace_arg_is_type_with_double_namespace_FullName()
+        {
+            Assert.Equal("A<N.O.A>", typeof(global::A<global::N.O.A>).GetFormattedFullName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_with_namespace_arg_is_type_with_double_namespace_Name()
+        {
+            Assert.Equal("A<A>", typeof(global::N.A<global::N.O.A>).GetFormattedName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_with_namespace_arg_is_type_with_double_namespace_FullName()
+        {
+            Assert.Equal("N.A<N.O.A>", typeof(global::N.A<global::N.O.A>).GetFormattedFullName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_with_double_namespace_arg_is_type_with_double_namespace_Name()
+        {
+            Assert.Equal("A<A>", typeof(global::N.O.A<global::N.O.A>).GetFormattedName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_with_double_namespace_arg_is_type_with_double_namespace_FullName()
+        {
+            Assert.Equal("N.O.A<N.O.A>", typeof(global::N.O.A<global::N.O.A>).GetFormattedFullName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_without_namespace_arg_is_generic_type_instantiation_Name()
+        {
+            Assert.Equal("A<A<A>>", typeof(global::A<global::A<global::A>>).GetFormattedName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_without_namespace_arg_is_generic_type_instantiation_FullName()
+        {
+            Assert.Equal("A<A<A>>", typeof(global::A<global::A<global::A>>).GetFormattedFullName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_with_namespace_arg_is_generic_type_instantiation_Name()
+        {
+            Assert.Equal("A<A<A>>", typeof(global::N.A<global::A<global::A>>).GetFormattedName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_with_namespace_arg_is_generic_type_instantiation_FullName()
+        {
+            Assert.Equal("N.A<A<A>>", typeof(global::N.A<global::A<global::A>>).GetFormattedFullName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_with_double_namespace_arg_is_generic_type_instantiation_Name()
+        {
+            Assert.Equal("A<A<A>>", typeof(global::N.O.A<global::A<global::A>>).GetFormattedName());
+        }
+
+        [Fact]
+        public void Generic_type_instantiation_with_double_namespace_arg_is_generic_type_instantiation_FullName()
+        {
+            Assert.Equal("N.O.A<A<A>>", typeof(global::N.O.A<global::A<global::A>>).GetFormattedFullName());
+        }
     }
 }
