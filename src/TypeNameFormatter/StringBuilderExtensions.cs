1a6c13b0a7368004aac77f554b8e55a9c5ba94f4
stakx <stakx@eml.cc>
2018-04-23 21:56:58 +0200

    Set up solution and an initial code skeleton

---
+++ src/TypeNameFormatter/StringBuilderExtensions.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) 2018 stakx
+// License available at https://github.com/stakx/TypeNameFormatter/blob/master/LICENSE.md.
+
+using System;
+using System.ComponentModel;
+using System.Text;
+
+namespace TypeNameFormatter
+{
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public static partial class StringBuilderExtensions
+    {
+        public static StringBuilder AppendName(this StringBuilder stringBuilder, Type type)
+        {
+            stringBuilder.AppendName(type, withNamespace: false);
+            return stringBuilder;
+        }
+
+        public static StringBuilder AppendFullName(this StringBuilder stringBuilder, Type type)
+        {
+            stringBuilder.AppendName(type, withNamespace: true);
+            return stringBuilder;
+        }
+
+        private static void AppendName(this StringBuilder stringBuilder, Type type, bool withNamespace)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
