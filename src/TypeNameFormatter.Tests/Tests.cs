cc445af73aafd41bd675922f0dc3a04a8dcbe5b9
stakx <stakx@eml.cc>
2018-04-25 14:18:01 +0200

    Regroup tests using `[Theory]`

--- src/TypeNameFormatter.Tests/Tests.cs
+++ src/TypeNameFormatter.Tests/Tests.cs
@@ -8,328 +8,119 @@ namespace TypeNameFormatter
 {
     public class Tests
     {
-        [Fact]
-        public void Type_without_namespace_Name()
+        [Theory]
+        [InlineData("A", typeof(global::A))]
+        [InlineData("A", typeof(global::N.A))]
+        [InlineData("A", typeof(global::N.O.A))]
+        public void Simple_type_Name(string expectedFormattedName, Type type)
         {
-            Assert.Equal("A", typeof(global::A).GetFormattedName());
+            Assert.Equal(expectedFormattedName, type.GetFormattedName());
         }
 
-        [Fact]
-        public void Type_without_namespace_FullName()
+        [Theory]
+        [InlineData("A", typeof(global::A))]
+        [InlineData("N.A", typeof(global::N.A))]
+        [InlineData("N.O.A", typeof(global::N.O.A))]
+        public void Simple_type_FullName(string expectedFormattedName, Type type)
         {
-            Assert.Equal("A", typeof(global::A).GetFormattedFullName());
+            Assert.Equal(expectedFormattedName, type.GetFormattedFullName());
         }
 
-        [Fact]
-        public void Type_with_namespace_Name()
+
+        [Theory]
+        [InlineData("A.B", typeof(global::A.B))]
+        [InlineData("A.B", typeof(global::N.A.B))]
+        [InlineData("A.B", typeof(global::N.O.A.B))]
+        [InlineData("A.B.C", typeof(global::A.B.C))]
+        [InlineData("A.B.C", typeof(global::N.A.B.C))]
+        [InlineData("A.B.C", typeof(global::N.O.A.B.C))]
+        public void Nested_type_Name(string expectedFormattedName, Type type)
         {
-            Assert.Equal("A", typeof(global::N.A).GetFormattedName());
+            Assert.Equal(expectedFormattedName, type.GetFormattedName());
         }
 
-        [Fact]
-        public void Type_with_namespace_FullName()
+        [Theory]
+        [InlineData("A.B", typeof(global::A.B))]
+        [InlineData("N.A.B", typeof(global::N.A.B))]
+        [InlineData("N.O.A.B", typeof(global::N.O.A.B))]
+        [InlineData("A.B.C", typeof(global::A.B.C))]
+        [InlineData("N.A.B.C", typeof(global::N.A.B.C))]
+        [InlineData("N.O.A.B.C", typeof(global::N.O.A.B.C))]
+        public void Nested_type_FullName(string expectedFormattedName, Type type)
         {
-            Assert.Equal("N.A", typeof(global::N.A).GetFormattedFullName());
+            Assert.Equal(expectedFormattedName, type.GetFormattedFullName());
         }
 
-        [Fact]
-        public void Type_with_double_namespace_Name()
+        [Theory]
+        [InlineData("A<T>", typeof(global::A<>))]
+        [InlineData("A<T>", typeof(global::N.A<>))]
+        [InlineData("A<T>", typeof(global::N.O.A<>))]
+        [InlineData("A<T, U>", typeof(global::A<,>))]
+        [InlineData("A<T, U>", typeof(global::N.A<,>))]
+        [InlineData("A<T, U>", typeof(global::N.O.A<,>))]
+        public void Generic_type_Name(string expectedFormattedName, Type type)
         {
-            Assert.Equal("A", typeof(global::N.O.A).GetFormattedName());
+            Assert.Equal(expectedFormattedName, type.GetFormattedName());
         }
 
-        [Fact]
-        public void Type_with_double_namespace_FullName()
+        [Theory]
+        [InlineData("A<T>", typeof(global::A<>))]
+        [InlineData("N.A<T>", typeof(global::N.A<>))]
+        [InlineData("N.O.A<T>", typeof(global::N.O.A<>))]
+        [InlineData("A<T, U>", typeof(global::A<,>))]
+        [InlineData("N.A<T, U>", typeof(global::N.A<,>))]
+        [InlineData("N.O.A<T, U>", typeof(global::N.O.A<,>))]
+        public void Generic_type_FullName(string expectedFormattedName, Type type)
         {
-            Assert.Equal("N.O.A", typeof(global::N.O.A).GetFormattedFullName());
+            Assert.Equal(expectedFormattedName, type.GetFormattedFullName());
         }
 
-        [Fact]
-        public void Nested_type_without_namespace_Name()
+        [Theory]
+        [InlineData("A<A>", typeof(global::A<global::A>))]
+        [InlineData("A<A>", typeof(global::N.A<global::A>))]
+        [InlineData("A<A>", typeof(global::N.O.A<global::A>))]
+        [InlineData("A<A>", typeof(global::A<global::N.A>))]
+        [InlineData("A<A>", typeof(global::N.A<global::N.A>))]
+        [InlineData("A<A>", typeof(global::N.O.A<global::N.A>))]
+        [InlineData("A<A>", typeof(global::A<global::N.O.A>))]
+        [InlineData("A<A>", typeof(global::N.A<global::N.O.A>))]
+        [InlineData("A<A>", typeof(global::N.O.A<global::N.O.A>))]
+        public void Generic_type_instantiation_Name(string expectedFormattedName, Type type)
         {
-            Assert.Equal("A.B", typeof(global::A.B).GetFormattedName());
+            Assert.Equal(expectedFormattedName, type.GetFormattedName());
         }
 
-        [Fact]
-        public void Nested_type_without_namespace_FullName()
+        [Theory]
+        [InlineData("A<A>", typeof(global::A<global::A>))]
+        [InlineData("N.A<A>", typeof(global::N.A<global::A>))]
+        [InlineData("N.O.A<A>", typeof(global::N.O.A<global::A>))]
+        [InlineData("A<N.A>", typeof(global::A<global::N.A>))]
+        [InlineData("N.A<N.A>", typeof(global::N.A<global::N.A>))]
+        [InlineData("N.O.A<N.A>", typeof(global::N.O.A<global::N.A>))]
+        [InlineData("A<N.O.A>", typeof(global::A<global::N.O.A>))]
+        [InlineData("N.A<N.O.A>", typeof(global::N.A<global::N.O.A>))]
+        [InlineData("N.O.A<N.O.A>", typeof(global::N.O.A<global::N.O.A>))]
+        public void Generic_type_instantiation_FullName(string expectedFormattedName, Type type)
         {
-            Assert.Equal("A.B", typeof(global::A.B).GetFormattedFullName());
+            Assert.Equal(expectedFormattedName, type.GetFormattedFullName());
         }
 
-        [Fact]
-        public void Nested_type_with_namespace_Name()
+        [Theory]
+        [InlineData("A<A<A>>", typeof(global::A<global::A<global::A>>))]
+        [InlineData("A<A<A>>", typeof(global::N.A<global::A<global::A>>))]
+        [InlineData("A<A<A>>", typeof(global::N.O.A<global::A<global::A>>))]
+        public void Recursive_generic_type_instantiation_Name(string expectedFormattedName, Type type)
         {
-            Assert.Equal("A.B", typeof(global::N.A.B).GetFormattedName());
+            Assert.Equal(expectedFormattedName, type.GetFormattedName());
         }
 
-        [Fact]
-        public void Nested_type_with_namespace_FullName()
+        [Theory]
+        [InlineData("A<A<A>>", typeof(global::A<global::A<global::A>>))]
+        [InlineData("N.A<A<A>>", typeof(global::N.A<global::A<global::A>>))]
+        [InlineData("N.O.A<A<A>>", typeof(global::N.O.A<global::A<global::A>>))]
+        public void Recursive_generic_type_instantiation_FullName(string expectedFormattedName, Type type)
         {
-            Assert.Equal("N.A.B", typeof(global::N.A.B).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Nested_type_with_double_namespace_Name()
-        {
-            Assert.Equal("A.B", typeof(global::N.O.A.B).GetFormattedName());
-        }
-
-        [Fact]
-        public void Nested_type_with_double_namespace_FullName()
-        {
-            Assert.Equal("N.O.A.B", typeof(global::N.O.A.B).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Double_nested_type_without_namespace_Name()
-        {
-            Assert.Equal("A.B.C", typeof(global::A.B.C).GetFormattedName());
-        }
-
-        [Fact]
-        public void Double_nested_type_without_namespace_FullName()
-        {
-            Assert.Equal("A.B.C", typeof(global::A.B.C).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Double_nested_type_with_namespace_Name()
-        {
-            Assert.Equal("A.B.C", typeof(global::N.A.B.C).GetFormattedName());
-        }
-
-        [Fact]
-        public void Double_nested_type_with_namespace_FullName()
-        {
-            Assert.Equal("N.A.B.C", typeof(global::N.A.B.C).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Double_nested_type_with_double_namespace_Name()
-        {
-            Assert.Equal("A.B.C", typeof(global::N.O.A.B.C).GetFormattedName());
-        }
-
-        [Fact]
-        public void Double_nested_type_with_double_namespace_FullName()
-        {
-            Assert.Equal("N.O.A.B.C", typeof(global::N.O.A.B.C).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Generic_type_without_namespace_Name()
-        {
-            Assert.Equal("A<T>", typeof(global::A<>).GetFormattedName());
-        }
-
-        [Fact]
-        public void Generic_type_without_namespace_FullName()
-        {
-            Assert.Equal("A<T>", typeof(global::A<>).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Generic_type_with_namespace_Name()
-        {
-            Assert.Equal("A<T>", typeof(global::N.A<>).GetFormattedName());
-        }
-
-        [Fact]
-        public void Generic_type_with_namespace_FullName()
-        {
-            Assert.Equal("N.A<T>", typeof(global::N.A<>).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Generic_type_with_double_namespace_Name()
-        {
-            Assert.Equal("A<T>", typeof(global::N.O.A<>).GetFormattedName());
-        }
-
-        [Fact]
-        public void Generic_type_with_double_namespace_FullName()
-        {
-            Assert.Equal("N.O.A<T>", typeof(global::N.O.A<>).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Generic_type_with_two_parameters_without_namespace_Name()
-        {
-            Assert.Equal("A<T, U>", typeof(global::A<,>).GetFormattedName());
-        }
-
-        [Fact]
-        public void Generic_type_with_two_parameters_without_namespace_FullName()
-        {
-            Assert.Equal("A<T, U>", typeof(global::A<,>).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Generic_type_with_two_parameters_with_namespace_Name()
-        {
-            Assert.Equal("A<T, U>", typeof(global::N.A<,>).GetFormattedName());
-        }
-
-        [Fact]
-        public void Generic_type_with_two_parameters_with_namespace_FullName()
-        {
-            Assert.Equal("N.A<T, U>", typeof(global::N.A<,>).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Generic_type_with_two_parameters_with_double_namespace_Name()
-        {
-            Assert.Equal("A<T, U>", typeof(global::N.O.A<,>).GetFormattedName());
-        }
-
-        [Fact]
-        public void Generic_type_with_two_parameters_with_double_namespace_FullName()
-        {
-            Assert.Equal("N.O.A<T, U>", typeof(global::N.O.A<,>).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_without_namespace_arg_is_type_without_namespace_Name()
-        {
-            Assert.Equal("A<A>", typeof(global::A<global::A>).GetFormattedName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_without_namespace_arg_is_type_without_namespace_FullName()
-        {
-            Assert.Equal("A<A>", typeof(global::A<global::A>).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_with_namespace_arg_is_type_without_namespace_Name()
-        {
-            Assert.Equal("A<A>", typeof(global::N.A<global::A>).GetFormattedName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_with_namespace_arg_is_type_without_namespace_FullName()
-        {
-            Assert.Equal("N.A<A>", typeof(global::N.A<global::A>).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_with_double_namespace_arg_is_type_without_namespace_Name()
-        {
-            Assert.Equal("A<A>", typeof(global::N.O.A<global::A>).GetFormattedName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_with_double_namespace_arg_is_type_without_namespace_FullName()
-        {
-            Assert.Equal("N.O.A<A>", typeof(global::N.O.A<global::A>).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_without_namespace_arg_is_type_with_namespace_Name()
-        {
-            Assert.Equal("A<A>", typeof(global::A<global::N.A>).GetFormattedName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_without_namespace_arg_is_type_with_namespace_FullName()
-        {
-            Assert.Equal("A<N.A>", typeof(global::A<global::N.A>).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_with_namespace_arg_is_type_with_namespace_Name()
-        {
-            Assert.Equal("A<A>", typeof(global::N.A<global::N.A>).GetFormattedName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_with_namespace_arg_is_type_with_namespace_FullName()
-        {
-            Assert.Equal("N.A<N.A>", typeof(global::N.A<global::N.A>).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_with_double_namespace_arg_is_type_with_namespace_Name()
-        {
-            Assert.Equal("A<A>", typeof(global::N.O.A<global::N.A>).GetFormattedName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_with_double_namespace_arg_is_type_with_namespace_FullName()
-        {
-            Assert.Equal("N.O.A<N.A>", typeof(global::N.O.A<global::N.A>).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_without_namespace_arg_is_type_with_double_namespace_Name()
-        {
-            Assert.Equal("A<A>", typeof(global::A<global::N.O.A>).GetFormattedName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_without_namespace_arg_is_type_with_double_namespace_FullName()
-        {
-            Assert.Equal("A<N.O.A>", typeof(global::A<global::N.O.A>).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_with_namespace_arg_is_type_with_double_namespace_Name()
-        {
-            Assert.Equal("A<A>", typeof(global::N.A<global::N.O.A>).GetFormattedName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_with_namespace_arg_is_type_with_double_namespace_FullName()
-        {
-            Assert.Equal("N.A<N.O.A>", typeof(global::N.A<global::N.O.A>).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_with_double_namespace_arg_is_type_with_double_namespace_Name()
-        {
-            Assert.Equal("A<A>", typeof(global::N.O.A<global::N.O.A>).GetFormattedName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_with_double_namespace_arg_is_type_with_double_namespace_FullName()
-        {
-            Assert.Equal("N.O.A<N.O.A>", typeof(global::N.O.A<global::N.O.A>).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_without_namespace_arg_is_generic_type_instantiation_Name()
-        {
-            Assert.Equal("A<A<A>>", typeof(global::A<global::A<global::A>>).GetFormattedName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_without_namespace_arg_is_generic_type_instantiation_FullName()
-        {
-            Assert.Equal("A<A<A>>", typeof(global::A<global::A<global::A>>).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_with_namespace_arg_is_generic_type_instantiation_Name()
-        {
-            Assert.Equal("A<A<A>>", typeof(global::N.A<global::A<global::A>>).GetFormattedName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_with_namespace_arg_is_generic_type_instantiation_FullName()
-        {
-            Assert.Equal("N.A<A<A>>", typeof(global::N.A<global::A<global::A>>).GetFormattedFullName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_with_double_namespace_arg_is_generic_type_instantiation_Name()
-        {
-            Assert.Equal("A<A<A>>", typeof(global::N.O.A<global::A<global::A>>).GetFormattedName());
-        }
-
-        [Fact]
-        public void Generic_type_instantiation_with_double_namespace_arg_is_generic_type_instantiation_FullName()
-        {
-            Assert.Equal("N.O.A<A<A>>", typeof(global::N.O.A<global::A<global::A>>).GetFormattedFullName());
+            Assert.Equal(expectedFormattedName, type.GetFormattedFullName());
         }
 
         [Theory]
@@ -357,15 +148,21 @@ namespace TypeNameFormatter
         [InlineData("A<T, U>.B<V, W>.C", typeof(global::A<,>.B<,>.C))]
         [InlineData("A<T, U>.B<V, W>.C<X>", typeof(global::A<,>.B<,>.C<>))]
         [InlineData("A<T, U>.B<V, W>.C<X, Y>", typeof(global::A<,>.B<,>.C<,>))]
-        public void Nested_closed_generic_types(string expectedName, Type type)
+        public void Nested_generic_type(string expectedName, Type type)
         {
             Assert.Equal(expectedName, type.GetFormattedName());
         }
 
         [Fact]
-        public void Nested_generic_type_instantiation_with_namespaces_in_type_args()
+        public void Nested_generic_type_instantiation_FullName()
         {
             Assert.Equal("A<A>.B<N.A, N.O.A>.C<A>", typeof(global::A<global::A>.B<global::N.A, global::N.O.A>.C<global::A>).GetFormattedFullName());
+        }
+
+        [Fact]
+        public void Nested_recursive_generic_type_instantiation_FullName()
+        {
+            Assert.Equal("A<A>.B<N.A<N.O.A<A>>, N.O.A>.C<A>", typeof(global::A<global::A>.B<global::N.A<global::N.O.A<global::A>>, global::N.O.A>.C<global::A>).GetFormattedFullName());
         }
     }
 }
