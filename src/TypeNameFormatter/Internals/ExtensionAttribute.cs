836d581c8fec3e0faa0cfeda100382cb0f826541
stakx <stakx@eml.cc>
2018-04-28 11:36:44 +0200

    Add `net20` framework target

---
+++ src/TypeNameFormatter/Internals/ExtensionAttribute.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) 2018 stakx
+// License available at https://github.com/stakx/TypeNameFormatter/blob/master/LICENSE.md.
+
+#if NET20
+namespace System.Runtime.CompilerServices
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    internal sealed class ExtensionAttribute : Attribute
+    {
+    }
+}
+#endif
