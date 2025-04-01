3626477094b57f6fcec0c10da2e5ff0bd18b79f0
Dominique Schuppli <stakx@eml.cc>
2025-04-01 19:53:23 +0200

    Update copyright notices to include contributors

--- src/TypeNameFormatter/TypeNameFormatter.Nullable.cs
+++ src/TypeNameFormatter/TypeNameFormatter.Nullable.cs
@@ -1,4 +1,4 @@
-// Copyright (c) 2018-2021 stakx
+// Copyright (c) 2018-2025 stakx and project contributors
 // License available at https://github.com/stakx/TypeNameFormatter/blob/main/LICENSE.md.
 
 // ReSharper disable All
